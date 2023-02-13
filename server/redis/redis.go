6a38edd3ac9cae57559273bcda1070e5fc3caf31
lizhuyou <wxlizhuyou@gf.com.cn>
2023-02-13 14:04:21 +0800

    添加redis和viper的应用

--- server/redis/redis.go
+++ server/redis/redis.go
@@ -1,83 +1,94 @@
 package myredis
 
 import (
-	configs "platform-backend/config"
-	utils "platform-backend/lib"
-	logger "platform-backend/server/log"
+	"context"
+	"github.com/go-redis/redis/v8"
+	utils "gitlab.gf.com.cn/hk-common/go-tool/lib"
 	"strings"
 	"time"
-
-	// "github.com/go-redis/redis"
-	"github.com/go-redis/redis/v7"
 )
 
 const (
-	REDISKEYNIL = "redis: nil"
+	REDISKEYNIL         = "redis: nil"
+	ZERO                = 0
+	DEFAULTTIMEOUT      = 15 * time.Second
+	DEFAULTDB           = 0
+	DEFAULTPOOLSIZE     = 10
+	DEFAULTLOCKTIME     = 10 * time.Second
+	DEFAULTLOOPLOCKTIME = 100 * time.Millisecond
 )
 
-var (
-	client *redis.Client
-)
-
-func init() {
-	redisHost := configs.GetConf("Redishost")
-	redisMaster := configs.GetConf("RedisMaster")
-	password := configs.GetConf("RedisMasterPassword")
-	sentinelPassword := configs.GetConf("RedisSentinelPassword")
-	hosts := strings.Split(redisHost, ",")
-	client = redis.NewFailoverClient(&redis.FailoverOptions{
-		MasterName:       redisMaster,
-		SentinelAddrs:    hosts,
-		Password:         password,
-		SentinelPassword: sentinelPassword,
-		ReadTimeout:      15 * time.Second,
-		WriteTimeout:     15 * time.Second,
-		DB:               0,
-		PoolSize:         10,
-		OnConnect:        OnConnect,
-	})
-}
-
-//OnConnect OnConnect
-func OnConnect(conn *redis.Conn) error {
-	_, err := conn.Ping().Result()
-	if err != nil {
-		logger.Error(err.Error())
-	}
-	return err
+type RedisConfig struct {
+	Host         string `json:"host"`
+	Master       string `json:"master"`
+	MPassword    string `json:"mpassword"`
+	SPassword    string `json:"spassword"`
+	Group        string `json:"group"` // 分组的key
+	ReadTimeout  int    `json:"readtimeout"`
+	WriteTimeout int    `json:"write_timeout"`
+	DB           int    `json:"db"`
+	PoolSize     int    `json:"poolsize"`
 }
 
 // RedisClient redis连接
 type RedisClient struct {
 	*redis.Client
-	LockTime time.Duration
-	lockKey  string
-	LockMap  map[string]string
+	prefixKey string
+	LockTime  time.Duration
+	lockKey   string
+	LockMap   map[string]string
 }
 
-// GetRedisClient 获取redis连接
-func GetRedisClient() *RedisClient {
-	// _, err := client.Ping().Result()
-	r := new(RedisClient)
-	r.Client = client
-	r.LockMap = make(map[string]string)
-	return r
+//OnConnect OnConnect
+func OnConnect(ctx context.Context, cn *redis.Conn) error {
+	_, err := cn.Ping(ctx).Result()
+	return err
 }
 
-// Nil redis查询空值错误
-func Nil() error {
-	return redis.Nil
+func NewRedisClient(config RedisConfig) *RedisClient {
+	op := &redis.FailoverOptions{
+		MasterName:       config.Master,
+		SentinelAddrs:    strings.Split(config.Host, ","),
+		Password:         config.MPassword,
+		SentinelPassword: config.SPassword,
+		ReadTimeout:      DEFAULTTIMEOUT,
+		WriteTimeout:     DEFAULTTIMEOUT,
+		DB:               DEFAULTDB,
+		PoolSize:         DEFAULTPOOLSIZE,
+		OnConnect:        OnConnect,
+	}
+	if config.ReadTimeout != ZERO {
+		op.ReadTimeout = time.Second * time.Duration(config.ReadTimeout)
+	}
+	if config.WriteTimeout != ZERO {
+		op.WriteTimeout = time.Second * time.Duration(config.WriteTimeout)
+	}
+	if config.PoolSize != ZERO {
+		op.PoolSize = config.PoolSize
+	}
+	if config.DB != ZERO {
+		op.PoolSize = config.PoolSize
+	}
+	client := redis.NewFailoverClient(op)
+	client.AddHook(NewHook(config.Group))
+	return &RedisClient{
+		Client:    client,
+		prefixKey: config.Group,
+		LockTime:  0,
+		lockKey:   "",
+		LockMap:   make(map[string]string),
+	}
 }
 
 // Lock 通过redis加锁
-func (r *RedisClient) Lock(id string) error {
-	if r.LockTime == 0 {
-		r.LockTime = 10 * time.Second
+func (r *RedisClient) Lock(ctx context.Context, id string) error {
+	if r.LockTime == ZERO {
+		r.LockTime = DEFAULTLOCKTIME
 	}
-	tc := time.NewTicker(100 * time.Millisecond) // 100毫秒尝试一次加锁
+	tc := time.NewTicker(DEFAULTLOOPLOCKTIME) // 100毫秒尝试一次加锁
 	for range tc.C {
 		lockValue := utils.Md5Sum(utils.GetRandomString(12))
-		f, err := r.Client.SetNX(id, lockValue, r.LockTime).Result()
+		f, err := r.Client.SetNX(ctx, id, lockValue, r.LockTime).Result()
 		if err != nil {
 			return err
 		}
@@ -90,12 +101,12 @@ func (r *RedisClient) Lock(id string) error {
 }
 
 // SingleLock 通过redis尝试一次加锁,成功返回true,失败返回false
-func (r *RedisClient) SingleLock(id string) (bool, error) {
-	if r.LockTime == 0 {
-		r.LockTime = 10 * time.Second
+func (r *RedisClient) SingleLock(ctx context.Context, id string) (bool, error) {
+	if r.LockTime == ZERO {
+		r.LockTime = DEFAULTLOCKTIME
 	}
 	lockValue := utils.Md5Sum(utils.GetRandomString(12))
-	f, err := r.Client.SetNX(id, lockValue, r.LockTime).Result()
+	f, err := r.Client.SetNX(ctx, id, lockValue, r.LockTime).Result()
 	if err == nil && f {
 		r.LockMap[id] = lockValue
 	}
@@ -103,26 +114,26 @@ func (r *RedisClient) SingleLock(id string) (bool, error) {
 }
 
 // Unlock 删除redis加锁（该方法不能判断是否解锁成功）
-func (r *RedisClient) Unlock(id string) error {
+func (r *RedisClient) Unlock(ctx context.Context, id string) error {
 	v, ok := r.LockMap[id]
 	if !ok { // lockMap中找不到值，解锁失败，直接返回
 		return nil
 	}
 
 	script := "if redis.call('get', KEYS[1]) == ARGV[1] then return redis.call('del', KEYS[1]) else return 0 end"
-	_, err := r.Eval(script, []string{id}, v).Result()
+	_, err := r.Eval(ctx, script, []string{id}, v).Result()
 	return err
 }
 
 // Unlock 删除redis加锁（返回是否解锁成功）
-func (r *RedisClient) UnlockWithFlag(id string) (bool, error) {
+func (r *RedisClient) UnlockWithFlag(ctx context.Context, id string) (bool, error) {
 	flag := false
 	v, ok := r.LockMap[id]
 	if !ok { // lockMap中找不到值，解锁失败，直接返回
 		return flag, nil
 	}
 	script := "if redis.call('get', KEYS[1]) == ARGV[1] then return redis.call('del', KEYS[1]) else return 0 end"
-	res, err := r.Eval(script, []string{id}, v).Result()
+	res, err := r.Eval(ctx, script, []string{id}, v).Result()
 	if err != nil {
 		return flag, err
 	}
@@ -133,7 +144,7 @@ func (r *RedisClient) UnlockWithFlag(id string) (bool, error) {
 	return flag, nil
 }
 
-// Nil redis Nil error
-func (r *RedisClient) Nil() error {
+// Nil redis查询空值错误
+func Nil() error {
 	return redis.Nil
 }
--- server/redis/redis_test.go
+++ server/redis/redis_test.go
@@ -1,128 +1,47 @@
 package myredis
 
 import (
+	"context"
 	"fmt"
-	"gopkg.in/ffmt.v1"
-	configs "platform-backend/config"
 	"testing"
 	"time"
 )
 
-func TestGetRedisClient(t *testing.T) {
-	cli := GetRedisClient()
-	result, err := cli.Set("b", "111", 1000*60*60*24).Result()
-	fmt.Printf("%s %#v\n", result, err)
-	result, err = cli.Get("b").Result()
-	fmt.Printf("%s %#v\n", result, err)
+var rdClient *RedisClient
+
+func init() {
+	op := RedisConfig{
+		Host:         "10.68.41.32:24377,10.68.41.33:24377,10.68.41.36:24377",
+		Master:       "mymaster",
+		MPassword:    "gfhs123456",
+		SPassword:    "gfhs123456",
+		Group:        "testdemo",
+		ReadTimeout:  0,
+		WriteTimeout: 0,
+		DB:           0,
+		PoolSize:     0,
+	}
+	rdClient = NewRedisClient(op)
+	rdClient.LockTime = 15 * time.Second
 }
 
-func TestDel(t *testing.T) {
-	cli := GetRedisClient()
-	id := "testsuyifeng111"
-	// v := "2341112"
-	err := cli.Lock(id)
+func TestNewRedisClient(t *testing.T) {
+	isLock, err := rdClient.SingleLock(context.Background(), "hahahah")
 	if err != nil {
 		t.Fatal(err)
 	}
-
-	cli.Lock(id)
-
-	cli1 := GetRedisClient()
-	err = cli1.Lock(id)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	err = cli1.Unlock(id)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	ffmt.Puts(err)
-
-	f, err := cli1.UnlockWithFlag(id)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	ffmt.Puts(f)
-
-	f, err = cli.UnlockWithFlag(id)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	ffmt.Puts(f)
-}
-
-func TestGetRedisClient2(t *testing.T) {
-	cli := GetRedisClient()
-	key := "token"
-	result, err := cli.Set(key, "cabc033f-d91a-4450-a223-4b031e1092bc", 1000*60*60*24).Result()
-	cli.Expire(key, 11*time.Second)
-	fmt.Printf("%s %#v\n", result, err)
-}
-
-func TestTokenIsOver(t *testing.T) {
-	cli := GetRedisClient()
-
-	expire, err := cli.TTL("token").Result()
-	over := expire.Milliseconds() / 1000
-	if over < 1 {
-		t.Fatal("timeout, please create")
-	}
-	fmt.Printf("expire=%d err =%v\n", over, err)
-
-	result, err := cli.Get("token").Result()
-	if err != nil {
-		fmt.Println("获取失败 err =", err)
-	}
-	fmt.Println(result)
-}
-
-func TestRedisClient_LockOne(t *testing.T) {
-	cli := GetRedisClient()
-	err := cli.Lock("test1.aa.1111")
-	if err != nil {
-		t.Fatal(err)
+	if !isLock {
+		t.Fatal("加锁失败")
 	}
 	fmt.Println("加锁成功")
-	defer func() {
-		cli.Lock("test1.aa.1111")
-		fmt.Println("加锁成功")
-	}()
-	err = cli.Unlock("test1.aa.1111")
-	if err != nil {
+	time.Sleep(5 * time.Second)
+	if _, err := rdClient.UnlockWithFlag(context.Background(), "hahahah"); err != nil {
 		t.Fatal(err)
 	}
 	fmt.Println("解锁成功")
 }
 
-func TestRedisClient_SingleLock(t *testing.T) {
-	cli := GetRedisClient()
-	f, err := cli.SingleLock("test1.aa.1111")
-	if err != nil {
-		t.Fatal()
-	}
-	if !f {
-		fmt.Println("加锁失败")
-		return
-	}
-	fmt.Println("加锁成功")
-	f, err = cli.SingleLock("test1.aa.1111")
-	if err != nil {
-		t.Fatal()
-	}
-	if !f {
-		fmt.Println("加锁失败")
-		return
-	}
-}
-func TestGetToken(t *testing.T) {
-	cli := GetRedisClient()
-	token, err := cli.Get(configs.HS_DICT_TOKEN_REDIS_KEY).Result()
-	if err != nil {
-		t.Fatal(err)
-	}
-	fmt.Println(token)
+// AddHook 添加中间件
+func TestAddHook(t *testing.T) {
+	rdClient.AddHook(NewHook("demo"))
 }
