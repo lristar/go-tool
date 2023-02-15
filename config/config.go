79f020d974bc1d1569f3e4c06270ce971c32e2f3
lizhuyou <wxlizhuyou@gf.com.cn>
2023-02-15 10:14:08 +0800

    viper添加监听配置文件

--- config/config.go
+++ config/config.go
@@ -2,11 +2,13 @@ package config
 
 import (
 	"fmt"
+	"github.com/fsnotify/fsnotify"
 	"github.com/mitchellh/mapstructure"
 	"github.com/spf13/viper"
 	"net/url"
 	"os"
 	"strings"
+	"time"
 )
 
 var (
@@ -25,8 +27,11 @@ type (
 	options struct {
 		env                  bool
 		openRemoteConfig     bool
+		configName           string
+		configPath           []string
 		remoteConfigEndPoint []string
 		defaultTag           string
+		openWatch            bool
 	}
 )
 
@@ -47,6 +52,19 @@ func UseCloseEnv() Option {
 	}
 }
 
+// AddConfigPath 添加额外的配置文件所包含的目录
+func AddConfigPath(configPath ...string) Option {
+	return func(opt *options) {
+		opt.configPath = configPath
+	}
+}
+
+func SetConfigName(configName string) Option {
+	return func(opt *options) {
+		opt.configName = configName
+	}
+}
+
 func UseRemoteConfig(endPoint ...string) Option {
 	return func(opt *options) {
 		if len(endPoint) > 0 {
@@ -62,8 +80,21 @@ func ResetTag(tag string) Option {
 	}
 }
 
+func OpenWatch() Option {
+	return func(opt *options) {
+		opt.openWatch = true
+	}
+}
+
+type cfgLock interface {
+	// TryLock 尝试加锁
+	TryLock() error
+	// UnLock 需要会自动释放
+	UnLock() error
+}
+
 // Setup 载入配置文件
-func Setup(configName string, cfg interface{}, ops ...Option) error {
+func Setup(defaultPath string, cfg cfgLock, ops ...Option) error {
 	var urls *url.URL
 	var err error
 	opt := newOption()
@@ -71,12 +102,23 @@ func Setup(configName string, cfg interface{}, ops ...Option) error {
 		o(&opt)
 	}
 	v := viper.NewWithOptions()
+
+	v.SetConfigType("yaml")
+	v.AddConfigPath(defaultPath)
+
+	// 配置文件名称 如果文件名是aaa.yaml,这里就要设置configName=aaa(viper默认的文件名为config)
+	if opt.configName != "" {
+		v.SetConfigName(opt.configName)
+	}
+
+	//配置额外的文件位置
+	for i := range opt.configPath {
+		v.AddConfigPath(opt.configPath[i])
+	}
 	//自动获取全部的env加入到viper中。（如果环境变量多就全部加进来）默认别名和环境变量名一致
 	if opt.env {
 		v.AutomaticEnv()
 	}
-	//配置文件位置
-	v.SetConfigFile(configName)
 	//读文件到viper配置中
 	err = v.ReadInConfig()
 	if err != nil {
@@ -96,6 +138,21 @@ func Setup(configName string, cfg interface{}, ops ...Option) error {
 		if err = v.ReadRemoteConfig(); err != nil {
 			return err
 		}
+	}
+	// 是否开启监听
+	if opt.openWatch {
+		v.OnConfigChange(func(in fsnotify.Event) {
+			// 缓一下确认更改完文件
+			time.Sleep(10 * time.Second)
+			if err := cfg.TryLock(); err != nil {
+				return
+			}
+			defer cfg.UnLock()
+			if err = v.Unmarshal(cfg, opt.ResetTag); err != nil {
+				return
+			}
+		})
+		v.WatchConfig()
 	}
 	// 系列化成config对象
 	if err = v.Unmarshal(cfg, opt.ResetTag); err != nil {
