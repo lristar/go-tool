37357225ac80fe93f3b864f92972478ad639c4db
hkhuangyong <huangyong@gfgroup.com.hk>
2023-02-23 15:36:26 +0800

    feat: rabbitmq的客户端更新

--- rabbitmq/rabbitmq.go
+++ rabbitmq/rabbitmq.go
@@ -3,107 +3,195 @@ package rabbitmq
 import (
 	"fmt"
 	"log"
-	"os"
 	"sync"
 	"time"
 
 	"github.com/streadway/amqp"
 )
 
-const (
-	maxConn     = 10
-	maxCh       = 50
-	maxRetries  = 3
-	maxInterval = 60 // 最大重连间隔时间（秒）
-)
-
-type RabbitMQ struct {
-	connPool    chan *amqp.Connection
-	chPool      chan *amqp.Channel
-	connRetries chan *amqp.Connection
+type Rabbit struct {
+	url            string             // RabbitMQ连接字符串
+	conn           *amqp.Connection   // RabbitMQ连接
+	channelPool    chan *amqp.Channel // Channel连接池
+	channelNum     int                // Channel连接数
+	reconnectCount int                // 重连次数
+	reconnectMutex sync.RWMutex       // 重连锁
+	connected      bool               // 是否已连接
 }
 
-func NewRabbitMQ() *RabbitMQ {
-	return &RabbitMQ{
-		connPool:    make(chan *amqp.Connection, maxConn),
-		chPool:      make(chan *amqp.Channel, maxCh),
-		connRetries: make(chan *amqp.Connection),
+// NewRabbit 创建一个新的Rabbit实例
+func NewRabbit(url string, channelNum int) (*Rabbit, error) {
+	rabbit := &Rabbit{
+		url:         url,
+		channelPool: make(chan *amqp.Channel, channelNum),
+		channelNum:  channelNum,
+		connected:   false,
 	}
+
+	err := rabbit.reconnect()
+	if err != nil {
+		return nil, err
+	}
+
+	go rabbit.keepAlive()
+
+	return rabbit, nil
 }
 
-func (r *RabbitMQ) Start() {
-	var wg sync.WaitGroup
-	for i := 0; i < maxConn; i++ {
-		wg.Add(1)
-		go func() {
-			defer wg.Done()
-			for {
-				conn, err := amqp.Dial(os.Getenv("RABBITMQ_URL"))
-				if err != nil {
-					log.Printf("Failed to connect to RabbitMQ: %s", err)
-					time.Sleep(time.Second * 5)
-					continue
-				}
-				log.Printf("Connected to RabbitMQ")
-				r.connPool <- conn
-
-				// Reconnect if the connection is closed
-				err = <-conn.NotifyClose(make(chan *amqp.Error))
-				if err != nil {
-					log.Printf("Lost connection to RabbitMQ: %s", err)
-				}
-				r.connRetries <- conn
-			}
-		}()
-	}
-
-	go func() {
-		for {
-			select {
-			case conn := <-r.connPool:
-				for i := 0; i < maxCh; i++ {
-					ch, err := conn.Channel()
-					if err != nil {
-						log.Printf("Failed to create channel: %s", err)
-						break
-					}
-					r.chPool <- ch
-				}
-			case conn := <-r.connRetries:
-				for i := 0; i < maxRetries; i++ {
-					interval := time.Duration(i+1) * time.Second
-					if interval > maxInterval*time.Second {
-						interval = maxInterval * time.Second
-					}
-					time.Sleep(interval)
-					log.Printf("Trying to reconnect to RabbitMQ (%d/%d)", i+1, maxRetries)
-					err := conn.Close()
-					if err != nil {
-						log.Printf("Failed to close connection: %s", err)
-					}
-					break
+// keepAlive 维持RabbitMQ连接的心跳
+func (r *Rabbit) keepAlive() {
+	for {
+		select {
+		case <-time.After(5 * time.Second):
+			if r.connected {
+				if r.conn.IsClosed() {
+					r.reconnect()
 				}
 			}
 		}
-	}()
+	}
 }
 
-func (r *RabbitMQ) GetChannel() (*amqp.Channel, error) {
-	ch, ok := <-r.chPool
+// reconnect 重连RabbitMQ
+func (r *Rabbit) reconnect() error {
+	r.reconnectMutex.Lock()
+	defer r.reconnectMutex.Unlock()
+
+	if r.connected {
+		return nil
+	}
+
+	conn, err := amqp.Dial(r.url)
+	if err != nil {
+		return err
+	}
+
+	channelPool := make(chan *amqp.Channel, r.channelNum)
+
+	for i := 0; i < r.channelNum; i++ {
+		channel, err := conn.Channel()
+		if err != nil {
+			return err
+		}
+
+		channelPool <- channel
+	}
+
+	r.conn = conn
+	r.channelPool = channelPool
+	r.connected = true
+	r.reconnectCount = 0
+
+	return nil
+}
+
+// GetChannel 获取一个Channel连接
+func (r *Rabbit) GetChannel() (*amqp.Channel, error) {
+	if !r.connected {
+		return nil, fmt.Errorf("connection is closed")
+	}
+
+	channel, ok := <-r.channelPool
 	if !ok {
-		return nil, fmt.Errorf("channel pool closed")
+		return nil, fmt.Errorf("channel pool is closed")
 	}
-	return ch, nil
+
+	return channel, nil
 }
 
-func (r *RabbitMQ) ReleaseChannel(ch *amqp.Channel) {
-	select {
-	case r.chPool <- ch:
-	default:
-		ch.Close()
+// ReturnChannel 归还一个Channel连接
+func (r *Rabbit) ReturnChannel(channel *amqp.Channel) {
+	if !r.connected {
+		return
+	}
+
+	r.channelPool <- channel
+}
+
+// Close 关闭RabbitMQ连接
+func (r *Rabbit) Close() error {
+	r.reconnectMutex.Lock()
+	defer r.reconnectMutex.Unlock()
+
+	if !r.connected {
+		return nil
+	}
+
+	err := r.conn.Close()
+	if err != nil {
+		return err
+	}
+
+	r.connected = false
+
+	return nil
+}
+
+// Consume 消费RabbitMQ队列消息
+func (r *Rabbit) Consume(queueName string, handler func(msg []byte) error) error {
+	channel, err := r.GetChannel()
+	if err != nil {
+		return err
+	}
+	defer r.ReturnChannel(channel)
+
+	err = channel.Qos(1, 0, false)
+	if err != nil {
+		return err
+	}
+	msgs, err := channel.Consume(
+		queueName,
+		"",
+		false,
+		false,
+		false,
+		false,
+		nil,
+	)
+	if err != nil {
+		return err
+	}
+
+	for {
+		select {
+		case msg := <-msgs:
+			log.Printf("received message: %s", string(msg.Body))
+			err := handler(msg.Body)
+			if err != nil {
+				log.Printf("failed to handle message: %s, error: %s", string(msg.Body), err.Error())
+				return err
+			}
+			err = msg.Ack(false)
+			if err != nil {
+				log.Printf("failed to acknowledge message: %s, error: %s", string(msg.Body), err.Error())
+				return err
+			}
+		}
 	}
 }
 
-func (r *RabbitMQ) Close() {
-	close(r.connPool)
-	close(r.ch
+// Publish 发布消息到RabbitMQ队列
+func (r *Rabbit) Publish(queueName string, msg []byte) error {
+	channel, err := r.GetChannel()
+	if err != nil {
+		return err
+	}
+	defer r.ReturnChannel(channel)
+
+	err = channel.Publish(
+		"",
+		queueName,
+		false,
+		false,
+		amqp.Publishing{
+			DeliveryMode: amqp.Persistent,
+			ContentType:  "text/plain",
+			Body:         msg,
+		},
+	)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
--- rabbitmq/rabbitmq_test.go
+++ rabbitmq/rabbitmq_test.go
@@ -1,78 +1,21 @@
 package rabbitmq
 
 import (
+	"log"
 	"testing"
 )
 
 func TestRabbitMQ(t *testing.T) {
-	r := NewRabbitMQ()
-	if r == nil {
-		t.Fatalf("Failed to create RabbitMQ instance")
-	}
-
-	err := r.Start()
+	rabbit, err := NewRabbit("amqp://guest:guest@localhost:5672/", 5)
 	if err != nil {
-		t.Fatalf("Failed to start RabbitMQ: %s", err)
+		log.Fatalf("Failed to create rabbit instance: %s", err.Error())
 	}
 
-	ch, err := r.GetChannel()
+	defer rabbit.Close()
+
+	msg := []byte("Hello, RabbitMQ!")
+	err = rabbit.Publish("test_queue", msg)
 	if err != nil {
-		t.Fatalf("Failed to get channel: %s", err)
-	}
-
-	err = ch.ExchangeDeclare(
-		"test_exchange", // name
-		"fanout",        // type
-		true,            // durable
-		false,           // auto-deleted
-		false,           // internal
-		false,           // no-wait
-		nil,             // arguments
-	)
-	if err != nil {
-		t.Fatalf("Failed to declare exchange: %s", err)
-	}
-
-	_, err = ch.QueueDeclare(
-		"test_queue", // name
-		true,         // durable
-		false,        // delete when unused
-		false,        // exclusive
-		false,        // no-wait
-		nil,          // arguments
-	)
-	if err != nil {
-		t.Fatalf("Failed to declare queue: %s", err)
-	}
-
-	err = ch.QueueBind(
-		"test_queue",    // name
-		"",              // routing key
-		"test_exchange", // exchange
-		false,           // no-wait
-		nil,             // arguments
-	)
-	if err != nil {
-		t.Fatalf("Failed to bind queue: %s", err)
-	}
-
-	msg := "Hello, World!"
-	err = r.Publish("test_exchange", "", []byte(msg))
-	if err != nil {
-		t.Fatalf("Failed to publish message: %s", err)
-	}
-
-	received, err := r.Consume("test_queue")
-	if err != nil {
-		t.Fatalf("Failed to consume: %s", err)
-	}
-
-	if string(received.Body) != msg {
-		t.Fatalf("Received message does not match published message")
-	}
-
-	err = r.Close()
-	if err != nil {
-		t.Fatalf("Failed to close RabbitMQ: %s", err)
+		log.Fatalf("Failed to publish message: %s", err.Error())
 	}
 }
